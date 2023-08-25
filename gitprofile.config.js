6553c9f494513c59220435328291c07dd47518a9
Majelis Ta'lim Ainul Yaqin <143152616+ainulyaqin313@users.noreply.github.com>
2023-08-25 16:16:03 +0700

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -90,7 +90,7 @@ const config = {
   // Display blog posts from your medium or dev account. (Optional)
   blog: {
     source: 'medium', // medium | dev
-    username: 'mawta.ainulyaqin', // to hide blog section, keep it empty
+    username: '@mawta.ainulyaqin', // to hide blog section, keep it empty
     limit: 10, // How many posts to display. Max is 10.
   },
   googleAnalytics: {
